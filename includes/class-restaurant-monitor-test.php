e4c206a767967fa6124ea30e587b53d2b5352c80
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2025-01-03 10:01:49 +0000

    Update class-restaurant-monitor-test.php

--- includes/class-restaurant-monitor-test.php
+++ includes/class-restaurant-monitor-test.php
@@ -55,9 +55,9 @@ class Restaurant_Monitor_Test {
 
             if (isset($data['data']['availability'])) {
                 echo "\nFound availability data\n";
-                foreach ($data['data']['availability'] as $date) {
-                    foreach ($date as $slot) {
-                        print_r($slot . "\n");
+                foreach ($data['data']['availability'] as $booking_date) {
+                    foreach ($booking_date as $slot) {
+                        print_r($slot);
                         if (isset($slot['time'])) {
                             echo sprintf(
                                 "Time: %s - %s - %s\n",
